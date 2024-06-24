e2a105a20f05f710b00393ec85516416642f161f
Ibukun Bello <BelloI@Coronationmb.com>
2024-06-24 20:59:09 +0100

    fsF

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const http = require('http');
 const socketIo = require('socket.io');
 const axios = require('axios');
 
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors());
 const server = http.createServer(app);
 const io = socketIo(server);
 
